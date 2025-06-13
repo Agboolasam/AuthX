b08f8cb5922ed284d819a43738acd8f917df4a76
Agboola Samson <agboolasamson602@gmail.com>
2025-06-13 03:03:27 +0100

    Repository reset and front end init with existing backend

---
+++ AuthXfe/src/pages/Login.jsx
@@ -0,0 +1,42 @@
+import { NavLink } from "react-router-dom"
+import logo from '../assets/logo.jpg'
+
+function Login() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
+                <img src={logo} alt="Logo" className="mb-6 mx-auto h-25 " />
+                <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Login</h2>
+
+                <form className="flex flex-col gap-4">
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        required
+                        className="px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        required
+                        className="px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <button
+                        type="submit"
+                        className="bg-blue-600 text-white py-2 rounded font-semibold hover:bg-blue-700 transition"
+                    >
+                        Sign In
+                    </button>
+                </form>
+                <p className="mt-4 text-center text-gray-600">
+                    Dont have an account?{" "}
+                    <NavLink to="/register" className="text-blue-600 hover:underline">
+                        Log in
+                    </NavLink>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Login
